7d1ae57aef481ff9dc1822d772bb6dd874778511
czenge25 <czenge25@kentdenver.org>
2023-09-21 08:42:49 -0600

    Finished Node class

---
+++ Node/src/Node.java
@@ -0,0 +1,51 @@
+/*
+CZ
+9/26/23
+ */
+
+public class Node<T> {
+
+    private int index;
+    private T data;
+    private Node parent;
+    private Node child;
+
+    public Node() {
+
+    }
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public Node(T data, Node parent, Node child) {
+        this.data = data;
+        this.parent = parent;
+        this.child = child;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String toString(Node n) {
+        return "Node: " + n.getData().toString();
+    }
+
+    public boolean equals(Node n1, Node n2) {
+        return n1.equals(n2);
+    }
+
+}
