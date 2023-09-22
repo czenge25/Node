2ee9497a0e6a2cf61f35f274d318a5f60d2e9e18
czenge25 <czenge25@kentdenver.org>
2023-09-22 12:03:04 -0600

    Wrote comments

--- Node/src/Node.java
+++ Node/src/Node.java
@@ -1,42 +1,48 @@
 /*
 CZ
-9/26/23
+9/22/23
+HK
  */
-
 public class Node<T> {
 
-    private T data;
-    private Node parent;
-    private Node child;
+    private T data; //Data stored in the node
+    private Node parent; //Reference to the parent node
+    private Node child; //Reference to the child node
 
+    //Default constructor
     public Node() {
 
     }
 
+    //Constructor that sets data of the node
     public Node(T data) {
         this.data = data;
     }
 
+    //Constructor that sets data, parent, and child
     public Node(T data, Node parent, Node child) {
         this.data = data;
         this.parent = parent;
         this.child = child;
     }
 
+    //Returns the data stored in the node
     public T getData() {
         return data;
     }
 
+    //Sets the data for this node
     public void setData(T data) {
         this.data = data;
     }
 
+    //Provides a string representation of the node by converting its data to a string
     public String toString() {
         return "Node: " + this.data.toString();
     }
 
+    //Compares the data of the node with the data of another node 'n'
     public boolean equals(Node n) {
         return this.data.equals(n.getData());
     }
-
 }
--- Node/src/TestNode.java
+++ Node/src/TestNode.java
@@ -1,36 +1,46 @@
-import java.util.Random;
-
+/*
+CZ
+9/22/23
+HK
+ */
 public class TestNode {
     public static void main(String[] args) {
 
+        //Creates three nodes with different data values and uses all constructors
         Node n1 = new Node(2);
         Node n2 = new Node(4);
-        Node n3 = new Node(8,n1,n2);
+        Node n3 = new Node(8, n1, n2);
 
+        //Creates an array of nodes and initializes it with the created nodes
         Node[] list;
         list = new Node[]{n1, n2, n3};
 
+        //Prints the data stored in each node in the list
         for (int i = 0; i < list.length; i++) {
             System.out.println(list[i].getData());
         }
 
+        //Sets the data in each node to 10 and prints the updated values
         for (int i = 0; i < list.length; i++) {
             list[i].setData(10);
             System.out.println(list[i].getData());
         }
 
-        System.out.println(n1.equals(n2));
-        System.out.println(n2.equals(n3));
-        System.out.println(n1.equals(n3));
+        //Tests the Node equals method on each Node
+        System.out.println(n1.equals(n2));  //Checks if n1 is equal to n2
+        System.out.println(n2.equals(n3));  //Checks if n2 is equal to n3
+        System.out.println(n1.equals(n3));  //Checks if n1 is equal to n3
 
+        //Prints string representations of nodes
         for (int i = 0; i < list.length; i++) {
             System.out.println(list[i].toString());
         }
 
+        //Creates a node with a String data type
         Node n4 = new Node("Hello");
-        System.out.println(n4.toString());
-        System.out.println(n4.getData());
-        n4.setData(3.14159265358979);
-        System.out.println(n4.getData());
+        System.out.println(n4.toString());    //Prints string representation of n4
+        System.out.println(n4.getData());      //Prints data stored in n4
+        n4.setData(3.14159265358979);          //Changes data in n4 to a double of pi
+        System.out.println(n4.getData());      //Prints n4 again
     }
 }
